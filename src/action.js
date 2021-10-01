fab102d761f69e2c900bc0b96c971f7891089263
Martin Tichovský <tichovskymartin@gmail.com>
2021-10-01 14:08:43 +0200

    fff

--- src/action.js
+++ src/action.js
@@ -1,7 +1,6 @@
 const core = require("@actions/core");
 const github = require("@actions/github");
 
-export const 
 try {
   // `who-to-greet` input defined in action metadata file
   const nameToGreet = core.getInput("who-to-greet");
